a9645e9f0626598fa2c9ad7341c4e7a6adb6e9ff
hanyi <a12qingtian3@hotmail.com>
2018-06-24 13:22:45 +0800

    first commit

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,14 +3,13 @@
       <div class="Homeheader"><i class="iconfont icon-VIVO"></i></div>
       <div class="official"><img src="/static/img/official.png" alt="图片" style="width: 100%;height:100%"></div>
       <Home-Swipe></Home-Swipe>
+      <Map-Positioning></Map-Positioning>
       <Home-List></Home-List>
-      <HomeProductContainer :todos="todos"></HomeProductContainer>
-      <Home-Container :todos="todos"></Home-Container>
+      <Home-Service />
+      <!-- <HomeProductContainer :todos="todos"></HomeProductContainer>
+      <Home-Container :todos="todos"></Home-Container> -->
       <Home-Footer></Home-Footer>
   </div>
-  </div>
-</div>
-
 </template>
 
 <script>
@@ -19,9 +18,11 @@ import { Swipe, SwipeItem } from 'mint-ui';
 import { MessageBox } from 'mint-ui';
 import HomeSwipe from './component/HomeSwipe';
 import HomeList from './component/HomeList';
-import HomeContainer from './component/HomeContainer'
+// import HomeContainer from './component/HomeContainer'
 import HomeFooter from '../../pages/footer'
-import HomeProductContainer from './component/HomeProductContainer'
+// import HomeProductContainer from './component/HomeProductContainer'
+import MapPositioning from './component/MapPositioning'
+import HomeService from './component/HomeService'
 import axios from 'axios';
 import {  getGoods, getCategory, getWechatUserInfo, getWechatOAuth2UserInfo, getWechatOpenid,getAdPositionDetail} from '../../api/api'
 export default {
@@ -37,8 +38,11 @@ export default {
   components:{
     HomeSwipe,
     HomeList,
-    HomeContainer,
-    HomeFooter,HomeProductContainer
+    // HomeContainer,
+    HomeFooter,
+    // HomeProductContainer,
+    MapPositioning,
+    HomeService
   },
   mounted:function(){
     this.getData()
@@ -79,6 +83,7 @@ export default {
 <style>
   .Home{
     border-bottom: 10px;
+    margin-bottom: 1.75rem;
   }
   .Homeheader i{
     font-size: 0.55rem;
---
+++ src/pages/home/component/HomeService.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="HomeService">
+            <div>
+                
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+.HomeService
+    height 4rem;
+    margin 0.3rem;
+    background white;
+</style>
+
+<script>
+export default {
+
+}
+</script>
+
