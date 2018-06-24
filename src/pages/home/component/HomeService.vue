3429c7491bd451a4f0a0e5d5e157b0fb4a5efe0e
hanyi <a12qingtian3@hotmail.com>
2018-06-24 13:40:41 +0800

    11

--- src/pages/home/component/HomeService.vue
+++ src/pages/home/component/HomeService.vue
@@ -1,10 +1,25 @@
 <template>
     <div>
         <div class="HomeService">
-            <div>
-                
+            <div class="serviceTop">
+                <div>
+                    <h6>
+                        小西
+                    </h6>
+                    <p>
+                        总监
+                    </p>
+                </div>
+                <div>
+                    <h5>
+                        <span></span>
+                    </h5>
+                    <p>
+                        总监
+                    </p>
+                </div>
             </div>
-            <div>
+            <div class="serviceBottom">
 
             </div>
         </div>
@@ -16,6 +31,10 @@
     height 4rem;
     margin 0.3rem;
     background white;
+.serviceTop
+    height 1.5rem;
+    background #FEE2EA;
+    opacity 0.5;
 </style>
 
 <script>
