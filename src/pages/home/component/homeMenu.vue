94a1e67299768e54b3d94cbcfabb936613cdf9b8
9267435@qq.com <9267435@qq.com>
2018-07-26 23:38:25 +0800

    commit

--- src/pages/home/component/homeMenu.vue
+++ src/pages/home/component/homeMenu.vue
@@ -3,30 +3,11 @@
     <mt-swipe :speed="200" :continuous="false" :stopPropagation="false" :prevent="false" :auto="0">
       <mt-swipe-item>
         <ul class="tableTabs">
-          <li class="table-tabs-item">
+          <li class="table-tabs-item" v-for="(item,index) in items" @click="$router.push(`${item.targetUrl}`)">
             <i>
-              <img src="../../../assets/jd/images/applist (1).png" alt="">
+              <img :src="item.imageUrl" alt="">
             </i>
-            <p>关于我们</p>
-          </li>
-          <li class="table-tabs-item" >
-            <i>
-              <img src="../../../assets/jd/images/applist (2).png" alt="" @click="$router.push('/subscribe')" >
-            </i>
-            <p>立即预约</p>
-          </li>
-
-          <li class="table-tabs-item">
-            <i>
-              <img src="../../../assets/jd/images/applist (8).png" alt="" @click="$router.push('/coupon')">
-            </i>
-            <p>领券</p>
-          </li>
-          <li class="table-tabs-item">
-            <i>
-              <img src="../../../assets/jd/images/applist (6).png" alt="" @click="$router.push('/mytrade')">
-            </i>
-            <p>充值</p>
+            <p>{{item.name}}</p>
           </li>
         </ul>
       </mt-swipe-item>
@@ -36,7 +17,12 @@
 
 <script>
     export default {
-        name: "home-menu"
+        name: "home-menu",
+      props:{
+          items:{
+            type:Array
+          }
+      }
     }
 </script>
 
