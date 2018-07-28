4d3341c87075120a3f8ebe386c3dc3d8f2e4f427
9267435@qq.com <9267435@qq.com>
2018-07-28 15:45:37 +0800

    commit

--- src/pages/home/component/homeActiviti.vue
+++ src/pages/home/component/homeActiviti.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="floor">
     <div class="container-bg">
-      <img :src="item.containerBgUrl" alt="">
+      <img :src="item.containerBgUrl" alt="" @click="$router.push(`${item.containerBgUrl}`)">
     </div>
     <div class="activity_list">
       <div class="left_item">
-        <img :src="item.leftItemUrl" alt="">
+        <img :src="item.leftItemUrl" alt="" @click="$router.push(`${item.leftItemUrl}`)">
       </div>
       <div class="right_item">
         <div class="r_i_cell">
-          <img :src="item.rightItemFirstUrl" alt="">
+          <img :src="item.rightItemFirstUrl" alt="" @click="$router.push(`${item.rightItemFirstUrl}`)">
         </div>
         <div class="r_i_cell">
-          <img :src="item.rightItemSecondUrl" alt="">
+          <img :src="item.rightItemSecondUrl" alt="" @click="$router.push(`${item.rightItemSecondUrl}`)">
         </div>
       </div>
     </div>
