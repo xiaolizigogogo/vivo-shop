a9c9096c606e2800cca395884fbd1d1c34cde7cd
9267435@qq.com <9267435@qq.com>
2018-07-28 15:52:22 +0800

    commit

--- src/pages/home/component/homeActiviti.vue
+++ src/pages/home/component/homeActiviti.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="floor">
     <div class="container-bg">
-      <img :src="item.containerBgUrl" alt="" @click="$router.push(`${item.containerBgUrl}`)">
+      <img :src="item.containerBgUrl" alt="" @click="$router.push(`${item.containerBgTargetUrl}`)">
     </div>
     <div class="activity_list">
       <div class="left_item">
-        <img :src="item.leftItemUrl" alt="" @click="$router.push(`${item.leftItemUrl}`)">
+        <img :src="item.leftItemUrl" alt="" @click="$router.push(`${item.leftItemTargetUrl}`)">
       </div>
       <div class="right_item">
         <div class="r_i_cell">
-          <img :src="item.rightItemFirstUrl" alt="" @click="$router.push(`${item.rightItemFirstUrl}`)">
+          <img :src="item.rightItemFirstUrl" alt="" @click="$router.push(`${item.rightItemFirstTargetUrl}`)">
         </div>
         <div class="r_i_cell">
-          <img :src="item.rightItemSecondUrl" alt="" @click="$router.push(`${item.rightItemSecondUrl}`)">
+          <img :src="item.rightItemSecondUrl" alt="" @click="$router.push(`${item.rightItemSecondTargetUrl}`)">
         </div>
       </div>
     </div>
