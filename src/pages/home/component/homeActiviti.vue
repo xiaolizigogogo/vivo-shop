dda2f29ecb4d972776cbd2214e81568bdcd8dd10
9267435@qq.com <9267435@qq.com>
2018-07-28 11:47:13 +0800

    commit

--- src/pages/home/component/homeActiviti.vue
+++ src/pages/home/component/homeActiviti.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="floor">
     <div class="container-bg">
-      <img src="../../../assets/jd/images/floor1.jpg" alt="">
+      <img :src="item.containerBgUrl" alt="">
     </div>
     <div class="activity_list">
       <div class="left_item">
-        <img src="../../../assets/jd/images/floor (1).jpg" alt="">
+        <img :src="item.leftItemUrl" alt="">
       </div>
       <div class="right_item">
         <div class="r_i_cell">
-          <img src="../../../assets/jd/images/floor (2).jpg" alt="">
+          <img :src="item.rightItemFirstUrl" alt="">
         </div>
         <div class="r_i_cell">
-          <img src="../../../assets/jd/images/floor (3).jpg" alt="">
+          <img :src="item.rightItemSecondUrl" alt="">
         </div>
       </div>
     </div>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -457,8 +457,6 @@
 <template>
   <div id="jd" class="jd">
     <!-- 搜索栏 -->
-    <search-bar :Status="Status"  v-if="searchBarVisilbe && cmsData" />
-    <!-- 搜索栏 -->
     <load-more @translate-change="translateChange" style="width:100%;" @loadMore="infiniteCallback" :commad="commad" :param="recommendParam"
       :loadMoreIconVisible="false" ref="recommendLoadmore" v-if="cmsData">
       <!-- <scroller class="cmsScroll" refreshLayerColor="#999" refreshText="下拉刷新..." :snapping="true" :onRefresh="onRefreshCallback" ref="cmsScroll" width="100%" height="100%"> -->
