1255fac2bf395340503d9afe384405d44b574de8
9267435@qq.com <9267435@qq.com>
2018-07-29 12:05:24 +0800

    commit

--- src/pages/cms/articleDetail.vue
+++ src/pages/cms/articleDetail.vue
@@ -6,6 +6,11 @@
     margin-top: 5px;
   }
   .article-container {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
     .con {
       padding: 20px $padding;
       background: #fff;
