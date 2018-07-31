2b6d159d99b7cbca91d582d39c953d669d17974a
jiangmingli <jiangmingli@zytc.com>
2018-07-31 17:32:43 +0800

    commit

---
+++ src/pages/my/myQrcode.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <img :src="user.qrcodeUrl">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "my-qrcode.vue",
+        data:{
+            user:''
+        },
+        mounted(){
+          this.user=JSON.parse(localStorage.getItem("user"))
+          this.user.qrcodeUrl="https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket="+this.user.qrcodeUrl
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
